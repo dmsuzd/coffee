6c8d0dd57aa114e6fecee35e4bb0d490dba3f4ca
dmsuzd <dmitrijsuzdalov23@gmail.com>
2024-12-08 02:44:39 +0300

    Add 2

--- main.py
+++ main.py
@@ -1,33 +1,39 @@
 import sys
 import random
-from PyQt6 import QtWidgets, uic
+from PyQt6 import QtWidgets
 from PyQt6.QtWidgets import QMainWindow, QApplication
 from PyQt6.QtGui import QPainter, QColor
 
 
-class App(QMainWindow):
+class CircleApp(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.ui = uic.loadUi("UI.ui", self)
+        self.setGeometry(0, 0, 362, 494)
+        self.setWindowTitle("Circle Drawer")
+
+        self.pushButton = QtWidgets.QPushButton("Add Circle", self)
+        self.pushButton.setGeometry(40, 390, 271, 41)
+        self.pushButton.clicked.connect(self.add_circle)
+
         self.circles = []
-        self.ui.pushButton.clicked.connect(self.add_circle)
 
     def paintEvent(self, event):
         painter = QPainter(self)
         for circle in self.circles:
-            painter.setBrush(QColor(255, 255, 0))
+            painter.setBrush(circle[3])
             painter.drawEllipse(circle[0], circle[1], circle[2], circle[2])
 
     def add_circle(self):
         diameter = random.randint(20, 100)
         x = random.randint(0, self.width() - diameter)
         y = random.randint(0, self.height() - diameter)
-        self.circles.append((x, y, diameter))
+        color = QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        self.circles.append((x, y, diameter, color))
         self.update()
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = App()
+    window = CircleApp()
     window.show()
     sys.exit(app.exec())
